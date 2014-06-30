465712445abb8a6d19bbdfe0992d5286c74a7b89
EliasLuiz <eliasluizjr@gmail.com>
2014-06-30 15:28:04 -0300

    Template desmembrado em header, includes-basicos, menu-top e rodape. Adicionado arquivos dos cadastros, a serem organizados depois.

--- index.php
+++ index.php
@@ -1,10 +1,79 @@
 <?php
-
-    /* 
-    * To change this license header, choose License Headers in Project Properties.
-    * To change this template file, choose Tools | Templates
-    * and open the template in the editor.
-    */
     
-    require("/html/menu-top.html");
+    require "html/includes-basicos.html" ;
+    require "html/header.html" ;
+    require "html/menu-top.html" ;
+    
+?>
+<div id="templatemo_portfolio" class="section2">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="secHeader">
+              <h1 class="text-center">O seu Laboratório de Análises Clínicas</h1>
+            </div>
+            <div class="glView">
+              <div class="menuSwitch">
+                <ul>
+                  <li class="" category="cliente">Cliente</li>
+                  <li class="" category="medico">Médico</li>
+                  <li class="" category="associado">Associados</li>
+                </ul>
+              </div>
+
+              <div class="imgSwitch">
+                <div class="row">
+                  <div class="col-xs-6 col-sm-3 col-md-3 dbox prod-cnt cliente">
+                    <div class="itemCont">
+                      <a rel="nofollow" href="http://www.templatemo.com/page/1">
+                        <div class="thumb"><img class="img-responsive center-block" alt="Blue Gate" src="images/grids/img1.gif"></div>
+                        <div class="itemInfo">
+                          <h4>Teste de Paternidade</h4>
+                          <p>O "Teste de DNA" ou "Teste de investigação da Paternidade" geralmente são feitos através da análise do DNA encontrado nas amostras de sangue do suposto pai, mãe e filho. O teste baseia-se na comparação entre os genes que compõem o DNA. O grau de coincidência deve ser total, se não for, a legitimidade paterna é excluída. Se todos os genes coincidirem, então afirma-se que o suposto pai é verdadeiro com grau de certeza acima de 99,99%.</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-3 col-md-3 dbox prod-cnt cliente">
+                    <div class="itemCont">
+                      <a rel="nofollow" href="http://www.templatemo.com/page/2">
+                        <div class="thumb"><img class="img-responsive center-block" alt="Womens Stone" src="images/grids/img2.gif"></div>
+                        <div class="itemInfo">
+                          <h4>Teste do Pezinho</h4>
+                          <p>O Teste do Pezinho é um exame laboratorial simples que tem o objetivo de detectar precocemente doenças metabólicas, genéticas e infecciosas, que poderão causar lesões irreversíveis no bebê. Por ser realizado através da análise de amostras de sangue coletadas do calcanhar do recém-nascido, o exame ficou popularmente conhecido como Teste do Pezinho. </p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-3 col-md-3 dbox prod-cnt associado">
+                    <div class="itemCont">
+                      <a rel="nofollow" href="http://www.templatemo.com/page/3">
+                        <div class="thumb"><img class="img-responsive center-block" alt="Elves Past" src="images/grids/img3.jpg"></div>
+                        <div class="itemInfo">
+                          <h4>Seja um Associado</h4>
+                          <p>Seja um associado e desfrute de vantagens para você e sua empresa.</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-3 col-md-3 dbox prod-cnt medico">
+                    <div class="itemCont">
+                      <a rel="nofollow" href="http://www.templatemo.com/page/4">
+                        <div class="thumb"><img class="img-responsive center-block" alt="Silken Wife" src="images/grids/img4.jpg"></div>
+                        <div class="itemInfo">
+                          <h4>Controle Médico</h4>
+                          <p>Analise os exames de seus pacientes de forma prática.</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>                
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- e/o section2 -->
+<?php
+    require "html/rodape.html";
+    
 ?>
