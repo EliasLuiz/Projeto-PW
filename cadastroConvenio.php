add57cb061e5c15ee4597a9d9d5d77785fab9a5d
EliasLuiz <eliasluizjr@gmail.com>
2014-07-08 21:09:39 -0300

    Adicionado as classes de interface para convenio e cliente

--- cadastroConvenio.php
+++ cadastroConvenio.php
@@ -1,5 +1,34 @@
 <?php
 
+    include_once 'Convenio.php';
+
     require_once 'html/cadastros/administrador/cadastroconvenios.html';
 
+    if(isset($_POST["nomeconvenio"])){
+        $interf = new InterfaceConvenio();
+        $convenio = $interf->gera();
+        $convenio->salvaMySQL();
+    }
+    
+    class InterfaceUsuario{
+        protected $nome;
+        protected $responsavel;
+        
+        function __construct() {
+            $this->nome = $_POST["nomeconvenio"];
+            $this->responsavel = $_POST["responsavel"];
+        }
+        
+        //Funções para validação aqui
+        
+        public function gera(){
+            
+            $convenio = new Convenio();
+            
+            $convenio->setNome($this->nome);
+            $convenio->setResponsavel($this->responsavel);
+            
+            return $convenio;
+        }
+    }
 ?>
