9b1d9e7216fce5fef4eedc21b68dea05d65e435a
EliasLuiz <eliasluizjr@gmail.com>
2014-07-08 21:37:08 -0300

    Adicionado a classe de interface para hospital

--- cadastroHospital.php
+++ cadastroHospital.php
@@ -1,5 +1,37 @@
 <?php
 
+    include_once 'Hospital.php';
+
     require_once 'html/cadastros/administrador/cadastrohospitais.html';
 
+    if(isset($_POST["nomehosp"])){
+        $interf = new InterfaceHospital();
+        $hospital = $interf->gera();
+        $hospital->salvaMySQL();
+    }
+    
+    class InterfaceHospital{
+    
+        protected $nmhospital;
+        protected $telefone;
+        
+        function __construct() {
+            $this->nmhospital = $_POST["nomehosp"];
+            $this->telefone = $_POST["ddd"] . $_POST["telefone"];
+        }
+        
+        //Funções para validação aqui
+        
+        public function gera(){
+            
+            //if(!$this->valida){ ERRO }
+            
+            $hosp = new Hospital();
+            
+            $hosp->setNmHospital($this->nmhospital);
+            $hosp->setTelefone($this->telefone);
+            
+            return $hosp;
+        }
+    }
 ?>
--- cadastroPaciente.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    require_once 'html/cadastros/cadastrocliente/cadastropaciente.html';
-
-?>
